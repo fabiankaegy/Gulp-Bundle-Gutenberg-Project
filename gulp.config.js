0b0de823344609ec65a750285a95a9514bc0181d
fabiankaegy <fabian@arvernus.info>
2018-11-20 19:59:55 +0100

    Removed Translation

--- gulp.config.js
+++ gulp.config.js
@@ -1,15 +1,7 @@
 module.exports = {
 
-	// Translation options.
-	textDomain: 'textdomain', // Your textdomain here.
-	translationFile: 'textdomain.pot', // Name of the translation file.
-	translationDestination: './languages', // Where to save the translation files.
-	packageName: 'testdomain', // Package name.
-	bugReport: 'your_url.com', // Where can users report bugs.
-	lastTranslator: 'Your Name <your_email@email.com>', // Last translator Email ID.
-	team: 'Your Name <your_email@email.com>', // Team's Email ID.
-
-	// Copy Options
+	packageName: 'packageName',
+	
 	buildFiles: [ 
 		'**',
 		'!blocks/**', 
@@ -26,6 +18,7 @@ module.exports = {
 		'!.eslintignore', 
 		'!phpcs.xml.dist' 
 	],
+
 	buildDestination: './build',
 
 };
